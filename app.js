5df93cde5e77101baff33d899ced9e6032b33bdd
Nitish <nitishagrawal2022@gmail.com>
2025-07-11 00:57:49 +0530

    Connected to MONGO Cloud and ready to deploy using Render

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ServerErrors = require("./utils/ServerErrors.js");
 const session=require("express-session");
+const MongoStore = require('connect-mongo');
 const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStratergy=require("passport-local");
@@ -27,14 +28,28 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const DBUrl=process.env.ATLAS_URL;
 main().then(() => { console.log("Connection to databse succeeded") }).catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect(DBUrl);
 }
 
+const store= MongoStore.create({
+    mongoUrl: DBUrl,
+    crypto: {
+        secret: process.env.SESSION_SECRET,
+    },
+    touchAfter: 24 * 3600
+});
+
+store.on("error", ()=>{
+    console.log("Error in MONGO store", err);
+})
+
 const sessionOptions={
-    secret: "SecretCode",
+    store,
+    secret: process.env.SESSION_SECRET,
     resave : false,
     saveUninitialized : true,
     cookie:{
